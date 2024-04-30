e30387608cf6875542391bf7c83be69e6c883885
zjc <2782171149@qq.com>
2024-04-30 19:21:00 +0800

    feat: 初始化Next.js项目

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-center mt-8">
+        Welcome to ZJC's Next.js App
+      </h1>
+    </div>
+  );
+}
